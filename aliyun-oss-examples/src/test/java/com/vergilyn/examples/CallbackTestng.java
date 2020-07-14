6d9244c4efb6172e3fd3ea96212634b6e5afb5f8
vergilyn <vergilyn@vip.qq.com>
2020-07-14 10:36:26 +0800

    todo: '2020-07-14'.

---
+++ aliyun-oss-examples/src/test/java/com/vergilyn/examples/CallbackTestng.java
@@ -0,0 +1,14 @@
+package com.vergilyn.examples;
+
+/**
+ * <a href="https://help.aliyun.com/document_detail/31853.html">OSS 上传回调</a> <br>
+ * <a href="https://help.aliyun.com/document_detail/31989.html">Callback API参考</a>
+ * <a href="https://github.com/aliyun/aliyun-oss-java-sdk/blob/master/src/samples/CallbackSample.java">Github - CallbackSample.java</a>
+ *
+ * 备注：在请求OSS进行上传时，可以指定callback。并且在callback中指定callback-body。
+ * @author VergiLyn
+ * @date 2020-01-17
+ */
+public class CallbackTestng {
+
+}
